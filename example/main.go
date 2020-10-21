0a35b58613370d8d0e188b057a73210cdbce066d
Khurshidali Shiekh <khurshidali.shiekh@appdynamics.com>
2020-10-21 13:05:05 -0700

    Added stdout trace exporter, commented jaeger exporter

--- example/main.go
+++ example/main.go
@@ -8,40 +8,59 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"go.opentelemetry.io/otel/api/global"
-	"go.opentelemetry.io/otel/exporters/trace/jaeger"
-	"go.opentelemetry.io/otel/label"
+	"go.opentelemetry.io/otel/exporters/stdout"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-func initTracer() func() {
-	// Create and install Jaeger export pipeline
-	flush, err := jaeger.InstallNewPipeline(
-		jaeger.WithCollectorEndpoint("http://localhost:14268/api/traces"),
-		jaeger.WithProcess(jaeger.Process{
-			ServiceName: "ot-sql-demo",
-			Tags: []label.KeyValue{
-				label.String("exporter", "jaeger"),
-				label.Float64("float", 312.23),
-			},
-		}),
-		jaeger.WithSDK(&sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+// func initTracer() func() {
+// 	// Create and install Jaeger export pipeline
+// 	flush, err := jaeger.InstallNewPipeline(
+// 		jaeger.WithCollectorEndpoint("http://localhost:14268/api/traces"),
+// 		jaeger.WithProcess(jaeger.Process{
+// 			ServiceName: "ot-sql-demo",
+// 			Tags: []label.KeyValue{
+// 				label.String("exporter", "jaeger"),
+// 				label.Float64("float", 312.23),
+// 			},
+// 		}),
+// 		jaeger.WithSDK(&sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
+// 	)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	return func() {
-		flush()
+// 	return func() {
+// 		flush()
+// 	}
+// }
+
+func initTracer() func() {
+	var err error
+	exp, err := stdout.NewExporter(stdout.WithPrettyPrint())
+	if err != nil {
+		log.Panicf("failed to initialize stdout exporter %v\n", err)
+		return nil
 	}
+	bsp := sdktrace.NewBatchSpanProcessor(exp)
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithConfig(
+			sdktrace.Config{
+				DefaultSampler: sdktrace.AlwaysSample(),
+			},
+		),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	global.SetTracerProvider(tp)
+	return bsp.Shutdown
 }
 
 func main() {
 	fn := initTracer()
 	defer fn()
 
+	connString := "<user>:<pwd>@tcp(127.0.0.1:3306)/<dbname>"
 	ctx := context.Background()
-	db, err := otsql.Open(ctx, "mysql",
-		"<usename>:<pwd>@tcp(127.0.0.1:3306)/<dbname>")
+	db, err := otsql.Open(ctx, "mysql", connString)
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	go.opentelemetry.io/otel v0.13.0
+	go.opentelemetry.io/otel/exporters/stdout v0.13.0
 	go.opentelemetry.io/otel/exporters/trace/jaeger v0.13.0
 	go.opentelemetry.io/otel/sdk v0.13.0
 )
